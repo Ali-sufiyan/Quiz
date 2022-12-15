20c49657dd9c70df40001a3c30dd39c1f56228c8
Ali-sufiyan <115947671+Ali-sufiyan@users.noreply.github.com>
2022-12-15 17:41:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+function check(){
+    var score=0;
+    var q1_ans_1 = document.getElementById("q1-a1");
+    var q1_ans_2 = document.getElementById("q1-a2");
+    var right_ans_3 = document.getElementById("q1-a3");
+    var q1_ans_4 = document.getElementById("q1-a4");
+
+    if(right_ans_3.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+
+
+    /////////////////////////////////////
+
+    var right_ans_1 = document.getElementById("q2-a1");
+    var q2_ans_2 = document.getElementById("q2-a2");
+    var q2_ans_3 = document.getElementById("q2-a3");
+    var q2_ans_4 = document.getElementById("q2-a4");
+
+    if(right_ans_1.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q3_ans_1 = document.getElementById("q3-a1");
+    var right_ans_2 = document.getElementById("q3-a2");
+    var q3_ans_3 = document.getElementById("q3-a3");
+    var q3_ans_4 = document.getElementById("q3-a4");
+
+    if(right_ans_2.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q4_ans_1 = document.getElementById("q4-a1");
+    var q4_ans_2 = document.getElementById("q4-a2");
+    var right_ans_3 = document.getElementById("q4-a3");
+    var q4_ans_4 = document.getElementById("q4-a4");
+
+    if(right_ans_3.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q5_ans_1 = document.getElementById("q5-a1");
+    var q5_ans_2 = document.getElementById("q5-a2");
+    var q5_ans_3 = document.getElementById("q5-a3");
+    var right_ans_4 = document.getElementById("q5-a4");
+
+    if(right_ans_4.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q6_ans_3 = document.getElementById("q6-a3");
+    var q6_ans_1 = document.getElementById("q6-a1");
+    var q6_ans_2 = document.getElementById("q6-a2");
+    var right_ans_4 = document.getElementById("q6-a4");
+
+    if(right_ans_4.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q7_ans_1 = document.getElementById("q7-a1");
+    var q7_ans_2 = document.getElementById("q7-a2");
+    var right_ans_3 = document.getElementById("q7-a3");
+    var q7_ans_4 = document.getElementById("q7-a4");
+
+    if(right_ans_3.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q8_ans_1 = document.getElementById("q8-a1");
+    var q8_ans_2 = document.getElementById("q8-a2");
+    var q8_ans_3 = document.getElementById("q8-a3");
+    var right_ans_4 = document.getElementById("q8-a4");
+
+    if(right_ans_4.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var q9_ans_1 = document.getElementById("q9-a1");
+    var q9_ans_2 = document.getElementById("q9-a2");
+    var right_ans_3 = document.getElementById("q9-a3");
+    var q9_ans_4 = document.getElementById("q9-a4");
+
+    if(right_ans_3.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+    /////////////////////////////////////
+
+    var right_ans_1 = document.getElementById("q10-a1");
+    var q10_ans_2 = document.getElementById("q10-a2");
+    var q10_ans_3 = document.getElementById("q10-a3");
+    var q110_ans_4 = document.getElementById("q10-a4");
+
+    if(right_ans_1.checked == true){
+        score++;
+        console.log("Your answer is right");
+    }
+    else{
+        console.log("wrong answer");
+    }
+
+////////////////////////////////////////////////////////
+
+
+}
